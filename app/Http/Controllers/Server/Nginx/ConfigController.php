5523b08e58656c8e1b4dfbe6067d84eb5fc59f8a
Ilya Ignatev <silentwasd@gmail.com>
2024-12-13 17:31:54 +0300

    Nginx config update restart

--- app/Http/Controllers/Server/Nginx/ConfigController.php
+++ app/Http/Controllers/Server/Nginx/ConfigController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Server\Nginx;
 use App\Http\Controllers\Controller;
 use App\Models\Server;
 use App\Services\NginxService;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ConfigController extends Controller
 {
+    /**
+     * @throws Exception
+     */
     public function show(Server $server)
     {
         $nginx = new NginxService($server);
@@ -19,6 +23,9 @@ class ConfigController extends Controller
         ]);
     }
 
+    /**
+     * @throws Exception
+     */
     public function update(Request $request, Server $server)
     {
         $data = $request->validate([
@@ -27,7 +34,18 @@ class ConfigController extends Controller
 
         $nginx = new NginxService($server);
 
+        $oldConfig = $nginx->getConfig();
+
         $nginx->setConfig($data['config']);
+
+        $result = $nginx->checkSite();
+
+        if ($result !== true) {
+            $nginx->setConfig($oldConfig);
+            abort(500, "Nginx check failed:\n$result");
+        }
+
+        $nginx->restart();
 
         return new JsonResource([
             'config' => $nginx->getConfig()
